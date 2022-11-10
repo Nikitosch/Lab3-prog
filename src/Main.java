ec09decffb5e665b2eb9426c09e6873a4a075312
niki <nik_prof1@mail.ru>
2022-11-10 23:38:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+         StartDay startDay = new StartDay("Wash doggos", "Take Doggos to Barber" , "Take Doggos to Mistress"); // need to do
+         startDay.go();
+    }
+}
---
+++ src/StartDay.java
@@ -0,0 +1,15 @@
+public class StartDay{
+    String[] deals;
+    public StartDay(String ... di){
+        deals = di;
+    }
+    public void go(){
+        MrsMinoge Minoge = new MrsMinoge();
+        Human[] people = {new NoNaika(deals,Minoge), Minoge, new Barber()};
+        for (TypeOfDay typeOfDay: TypeOfDay.values()){
+            for (Human human: people){
+                human.startDoingSmt(typeOfDay);
+            }
+        }
+    }
+}
